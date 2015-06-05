e19ee44f94751a7d4c38dfcc734af0be3de6a5e9
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-06-05 17:45:52 +0100

    Github fetcher now works perfectly! Had to add organisation variable

--- spec/github_fetcher_spec.rb
+++ spec/github_fetcher_spec.rb
@@ -3,13 +3,13 @@ require './lib/github_fetcher'
 
 describe 'github_listener' do
 
-	let(:team_members_accounts) {["binaryberry", "boffbowsh"]}
-	let(:team_repos) {["CV", "bookmark-collector", "whitehall"]}
+	let(:team_members_accounts) {["binaryberry", "jamiecobbett", "boffbowsh", "alicebartlett", "benilovj", "fofr", "russellthorn", "tekin"]}
+	let(:team_repos) {["whitehall"]}
 	let(:github_fetcher) { GithubFetcher.new(team_members_accounts, team_repos) }
 
 	context "list_pull_requests" do
 		it "should display open pull requests open on the team's repos by a team member" do
-			expect(github_fetcher.list_pull_requests).to eq(["testing things", "minor change"])
+			expect(github_fetcher.list_pull_requests).to eq(["Remove redundant election-related code"])
 		end
 	end
 end
