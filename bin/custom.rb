4405c5e063db9f2b80d0b720ebaaa927c7e159b1
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-07-03 17:08:22 +0100

    WIP

--- bin/custom.rb
+++ bin/custom.rb
@@ -4,8 +4,6 @@ require "./lib/github_fetcher.rb"
 require "./lib/message_builder.rb"
 require "./lib/slack_poster.rb"
 
-puts "it runs!"
-
 TEAM_MEMBERS_ACCOUNTS = ["jennyd", "dsingleton", "tommyp", "issyl0", "mikejustdoit", "marksheldonGDS"]
 TEAM_REPOS = %w(calendars contacts-admin contacts-frontend content-register content-store courts-api courts-frontend hmrc-manuals-api imminence licence-finder manuals-frontend optic14n pre-transition-stats publishing-api side-by-side-browser smart-answers specialist-publisher static trade-tariff-admin trade-tariff-backend trade-tariff-frontend transition transition-stats url-arbiter whitehall).sort!
 TEAM_CHANNEL = "#custom"
--- lib/slack_poster.rb
+++ lib/slack_poster.rb
@@ -2,27 +2,44 @@ require 'slack-poster'
 
 class SlackPoster
 
-  attr_accessor :webhook_url, :poster
+  attr_accessor :webhook_url, :poster, :mood, :mood_hash, :channel
 
-  def initialize(webhook_url, team_channel)
-    if ENV["DYNO"].nil?
-      @poster = Slack::Poster.new("#{webhook_url}", options = {
-        icon_emoji: ':informative_seal:',
-        username: 'Informative Seal',
-        channel: '#angry-seal-bot-test'
-      })
-    else
-      @poster = Slack::Poster.new("#{webhook_url}", options = {
-        icon_emoji: ':informative_seal:',
-        username: 'Informative Seal',
-        channel: team_channel
-      })
-    end
-      @webhook_url = webhook_url
+  def initialize(webhook_url, team_channel, mood)
+    @webhook_url = webhook_url
+    @team_channel = team_channel
+    @mood = mood
+    mood_hash
+    channel
+    create_poster
+  end
+
+  def create_poster
+    @poster = Slack::Poster.new("#{webhook_url}", options = {
+      icon_emoji: @mood_hash[:icon_emoji],
+      username: @mood_hash[:username],
+      channel: @team_channel
+    })
   end
 
   def send_request(message)
     poster.send_message("#{message}") unless Date.today.saturday? || Date.today.sunday?
   end
 
+  private
+
+  def mood_hash
+    @mood_hash = []
+    if @mood == "Informative"
+      # @mood_hash[:icon_emoji]= ":send_request:"
+      mood_hash[:username]= "Informative Seal"
+    elsif @mood == "Angry"
+      puts "yeay"
+    else
+      raise "bad mood"
+    end
+  end
+
+  def channel
+    @team_channel = '#angry-seal-bot-test' if ENV["DYNO"].nil?
+  end
 end
