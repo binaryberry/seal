af05118e5da551e8b8aef4f3ccde07b5c29923b4
Isabell Long <isabell@issyl0.co.uk>
2015-07-07 09:51:30 +0100

    Add Business Support API and transition-config to Custom's apps
    - The daily roundup of PRs was missing one of mine because the
      Business Support API wasn't tracked.

--- bin/custom.rb
+++ bin/custom.rb
@@ -5,7 +5,7 @@ require "./lib/message_builder.rb"
 require "./lib/slack_poster.rb"
 
 TEAM_MEMBERS_ACCOUNTS = ["jennyd", "dsingleton", "tommyp", "issyl0", "mikejustdoit", "marksheldonGDS"]
-TEAM_REPOS = %w(calendars contacts-admin contacts-frontend content-register content-store courts-api courts-frontend hmrc-manuals-api imminence licence-finder manuals-frontend optic14n pre-transition-stats publishing-api side-by-side-browser smart-answers specialist-publisher static trade-tariff-admin trade-tariff-backend trade-tariff-frontend transition transition-stats url-arbiter whitehall).sort!
+TEAM_REPOS = %w(business-support-api calendars contacts-admin contacts-frontend content-register content-store courts-api courts-frontend hmrc-manuals-api imminence licence-finder manuals-frontend optic14n pre-transition-stats publishing-api side-by-side-browser smart-answers specialist-publisher static trade-tariff-admin trade-tariff-backend trade-tariff-frontend transition transition-config transition-stats url-arbiter whitehall).sort!
 TEAM_CHANNEL = "#custom"
 
 
