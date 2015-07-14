de349d22a60a98d33bdc009a3f6a0d08e4877921
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-07-14 10:14:57 +0100

    added different message when there are no pull requests, added repo to core-formats config

--- spec/message_builder_spec.rb
+++ spec/message_builder_spec.rb
@@ -3,11 +3,17 @@ require './lib/message_builder'
 
 describe MessageBuilder do
   let(:pull_requests)   { {"[review but do not merge]Rename Topic to PolicyArea in Publisher UI"=>{"title"=>"[review but do not merge]Rename Topic to PolicyArea in Publisher UI", "link"=>"https://github.com/alphagov/whitehall/pull/2249", "author"=>"jackscotti", "repo"=>"whitehall", "comments_count"=>"1"}}}
-  let(:message_builder) { MessageBuilder.new(pull_requests) }
+  let(:empty_pull_request) {{}}
 
   context "build" do
     it "builds the body of the message to be posted on Slack" do
-      expect(message_builder.build).to eq("Good morning team! Here are the pull requests that need to be reviewed today:\n\n1) *whitehall* | jackscotti\n<https://github.com/alphagov/whitehall/pull/2249|[review but do not merge]Rename Topic to PolicyArea in Publisher UI> - 1 comment\n\nMerry reviewing!")
+      message_builder = MessageBuilder.new(pull_requests)
+      expect(message_builder.build).to eq("Good morning team! \n\n Here are the pull requests that need to be reviewed today:\n\n1) *whitehall* | jackscotti\n<https://github.com/alphagov/whitehall/pull/2249|[review but do not merge]Rename Topic to PolicyArea in Publisher UI> - 1 comment\n\nMerry reviewing!")
+    end
+
+    it "displays a happy message when there are no pull requests" do
+      message_builder = MessageBuilder.new(empty_pull_request)
+      expect(message_builder.build).to eq("Good morning team! It's a beautiful day! :happyseal: :happyseal: :happyseal:\n\nNo pull requests to review today! :rainbow: :sunny: :metal: :tada:")
     end
   end
 
