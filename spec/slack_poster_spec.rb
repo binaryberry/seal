f08b464ffddac5d674929cc848b4be1d27ba72bb
Pete Johns <pete.johns@envato.com>
2015-07-21 13:49:03 +1000

    Stop Slack spec hitting the Internet

--- spec/slack_poster_spec.rb
+++ spec/slack_poster_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
 require './lib/slack_poster'
 
+describe 'slack_poster' do
+  subject(:slack_poster) { SlackPoster.new(webhook_url, team_channel, mood) }
 
-  describe 'slack_poster' do
+  let(:webhook_url) { 'https://slack/webhook' }
+  let(:team_channel) { '#angry-seal-bot-test' }
+  let(:message) { 'test running!' }
+  let(:mood) { 'Informative' }
+  let(:fake_slack_poster) { instance_double(Slack::Poster) }
 
-    let(:webhook_url)      { ENV["SLACK_WEBHOOK"]                              }
-    let(:team_channel)     { "#angry-seal-bot-test"                            }
-    let(:message)          { "test running!"                                   }
-    let(:mood)             { "Informative"                                     }
-    let(:slack_poster)     { SlackPoster.new(webhook_url, team_channel, mood ) }
-
-    context "send_request" do
-      it "should receive a 200 response code" do
-        expect(slack_poster.send_request(message)).to eq("ok (200)")
-      end
+  context 'send_request' do
+    before do
+      expect(Slack::Poster).to receive(:new).and_return(fake_slack_poster)
     end
 
+    it 'posts to Slack' do
+      expect(fake_slack_poster).to receive(:send_message).with(message).and_return(:ok)
+      expect(slack_poster.send_request(message)).to be :ok
+    end
   end
-
+end
