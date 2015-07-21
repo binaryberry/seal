d8e6dca75e14ed70203e8bae4ab1b29d7fa7dee4
Pete Johns <pete.johns@envato.com>
2015-07-21 10:07:24 +1000

    Let's not overwrite each other's configuration
    TODO: Does this belong in the same repository?

--- bin/angry_seal.rb
+++ bin/angry_seal.rb
@@ -5,7 +5,8 @@ require "./lib/github_fetcher.rb"
 require "./lib/message_builder.rb"
 require "./lib/slack_poster.rb"
 
-CONFIG = YAML.load_file( "./config/config.yml" )[ARGV[0]]
+ORGANISATION ||= ENV['SEAL_ORGANISATION']
+CONFIG = YAML.load_file("./config/#{ORGANISATION}.yml")[ARGV[0]]
 
 git = GithubFetcher.new(CONFIG["members"],CONFIG["repos"])
 
