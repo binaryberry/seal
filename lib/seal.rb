7fce9942d460f70b7a839041860f90da9ff8118e
Pete Johns <pete.johns@envato.com>
2015-07-21 15:34:28 +1000

    Move Seal entry point into a class

---
+++ lib/seal.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+require './lib/github_fetcher.rb'
+require './lib/message_builder.rb'
+require './lib/slack_poster.rb'
+
+# Entry point for the Seal!
+class Seal
+  ORGANISATION ||= ENV['SEAL_ORGANISATION']
+
+  def initialize(team, mood)
+    @team = team
+    @mood = mood
+  end
+
+  def bark
+    message_builder = MessageBuilder.new(pull_requests, mood)
+    message = message_builder.build
+    slack = SlackPoster.new(ENV['SLACK_WEBHOOK'], config['channel'], mood.capitalize)
+    slack.send_request(message)
+  end
+
+  private
+
+  attr_accessor :team, :mood
+
+  def config
+    @config ||= YAML.load_file("./config/#{ORGANISATION}.yml")[team]
+  end
+
+  def pull_requests
+    git = GithubFetcher.new(config['members'], config['repos'])
+    git.list_pull_requests
+  end
+end
