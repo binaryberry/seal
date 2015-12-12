7edfceeea19373ca37ebc7e1389d395d9c914225
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-12-12 17:03:42 +0000

    Add procfile and route params

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require './lib/github_fetcher'
 require './lib/message_builder'
 require './lib/slack_poster'
 
-get '/seal' do
-  Seal.new("core-formats").bark
+# POST /teams/core-formats
+post '/teams/?:team_name?' do
+  Seal.new(params[:team_name]).bark
 end
