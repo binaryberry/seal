84a1f34b44acc92d9f5d97ab1e369ad63e650d87
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-04-06 17:25:44 +0100

    Add script to run seal scripts
    There are so many jobs to update on heroku scheduler now that I've decided to batch
    them by time of day they run at. This will make time changes easier!

---
+++ bin/late_afternoon_seal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+`./bin/seal.rb tea quotes`
---
+++ bin/morning_seal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+`./bin/seal.rb core-formats`
+`./bin/seal.rb publishing-platform`
+`./bin/seal.rb specialist-publisher`
+`./bin/seal.rb finding-things`
+`./bin/seal.rb custom`
+`./bin/seal.rb govuk-infrastructure`
+`./bin/seal.rb servicemanual`
