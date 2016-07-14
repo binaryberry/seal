78a118e5dade873806b0399bd107afa6e1d3c2e0
Daniel Dignam <dan@grabcad.com>
2016-07-14 11:02:37 +0100

    Remove debug

--- lib/seal.rb
+++ lib/seal.rb
@@ -37,9 +37,6 @@ class Seal
     message_builder = MessageBuilder.new(team_params(team), @mode)
     message = message_builder.build
     channel = ENV["SLACK_CHANNEL"] ? ENV["SLACK_CHANNEL"] : team_config(team)['channel']
-    puts channel
-    puts ENV['SLACK_WEBHOOK']
-
     slack = SlackPoster.new(ENV['SLACK_WEBHOOK'], channel, message_builder.poster_mood)
     slack.send_request(message)
   end
