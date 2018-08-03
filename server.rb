69540cb87c55c0acd980e41fc810d3bec7e9ac79
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-08-03 10:31:21 +0100

    Fix calling of Seal via HTTP request
    This commit fixes `server.rb`, which handles HTTP requests to the Seal, following some earlier refactoring.

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require './lib/seal'
 require './lib/github_fetcher'
 require './lib/message_builder'
 require './lib/slack_poster'
+require './lib/team_builder'
 
 class SealApp < Sinatra::Base
 
@@ -12,13 +13,15 @@ class SealApp < Sinatra::Base
 
   post '/bark/:team_name/:secret' do
     if params[:secret] == ENV["SEAL_SECRET"]
-      Seal.new(params[:team_name]).bark
+      team = TeamBuilder.build(env: ENV, team_name: params[:team_name])
+      Seal.new(team).bark
       "Seal received message with #{params[:team_name]} team name"
     end
   end
 
   post '/bark-quotes/:team_name' do
-    Seal.new(params[:team_name], "quotes").bark
-    "Seal received message with #{params[:team_name]} team name"
+    team = TeamBuilder.build(env: ENV, team_name: params[:team_name])
+    Seal.new(team).bark(mode: 'quotes')
+    "Seal received message with #{params[:team_name]} team name for quotes"
   end
 end
