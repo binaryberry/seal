30ad445d7d80297773069b30e14df372fcb06ada
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-02-12 09:14:44 +0000

    Put the morning seal config into alphabetical order

--- bin/morning_seal.sh
+++ bin/morning_seal.sh
@@ -3,16 +3,16 @@
 teams=(
   design-system-dev
   digitalmarketplace
+  govuk-content-pages
   govuk-data-informed
   govuk-euexit-ready
   govuk-finding-brexit
   govuk-licensing
+  govuk-pay
   govuk-platform-health
   govuk-pub-workflow
-  govuk-content-pages
   re-autom8
   re-infra-internal
-  govuk-pay
 )
 
 for team in ${teams[*]}; do
